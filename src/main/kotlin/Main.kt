e3867610e20584c9c631f75374addc0c99ed24e1
qmk <wherlicq@gmail.com>
2023-11-06 16:19:00 +0100

    Added destination argument. Removed possibility to use current folder by setting no arguments.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,31 @@ import java.io.File
 fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    val path = try {
+    val sourcePath = try {
         args[0]
     } catch (e: Exception) {
         e.printStackTrace()
-        println("No args found, using current directory.")
-        System.getProperty("user.dir")
+        println("No args found.")
+        return
     }
-    val dir = File(path)
-    println(dir.path)
-    dir.walk().forEach {
+
+    val destinationPath = try {
+        args[1]
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("No destination found.")
+        return
+    }
+
+    val sourceDir = File(sourcePath)
+    val destinationDir = File(destinationPath)
+    println(sourceDir.path)
+    sourceDir.walk().forEach {
         println(it)
-        if (!it.isDirectory) {
+        if (!it.isDirectory && it.extension != "jar" && it.extension != "sh") {
             try {
-                it.copyTo(File("${dir.path}/${it.name}"))
+                it.copyTo(File("${destinationDir.path}/${it.name}"))
+                it.delete()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
